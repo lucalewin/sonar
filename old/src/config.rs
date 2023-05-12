61e7feae7d0468055b220dacc40d53f67bc2461c
Luca Lewin <luca.lewin23@gmail.com>
2023-05-12 23:08:37 +0200

    remove old code

--- old/src/config.rs
+++
@@ -1,5 +0,0 @@
-// pub const DEFAULT_CONFIG: Config = Config { port: 8764 };
-
-// pub struct Config {
-//     pub port: i16,
-// }
--- old/src/devices.rs
+++
@@ -1,6 +0,0 @@
-// use std::net::IpAddr;
-
-// struct Device {
-//     ip: IpAddr,
-//     name: String,
-// }
