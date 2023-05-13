87c1cb9e05348215dd5bfe4aa84512f61c21d950
Luca Lewin <luca.lewin23@gmail.com>
2023-05-13 18:02:00 +0200

    remove unused functions

--- src/audio/devices.rs
+++ src/audio/devices.rs
@@ -1,4 +1,4 @@
-use cpal::traits::{DeviceTrait, HostTrait};
+use cpal::traits::DeviceTrait;
 use log::debug;
 
 use super::WavData;
@@ -10,11 +10,11 @@ pub enum Device {
 }
 
 impl Device {
-    // Construct a [Device] from a [cpal::Device].
-    //
-    // Devices may support both input and output.
-    // This defaults to output if both are present on one device.
-    fn from_device(device: cpal::Device) -> Option<Self> {
+    /// Construct a [Device] from a [cpal::Device].
+    ///
+    /// Devices may support both input and output.
+    /// This defaults to output if both are present on one device.
+    pub fn from_device(device: cpal::Device) -> Option<Self> {
         // Only use the default config for output or input
         // Prefer output if a device supports both
         if let Ok(conf) = device.default_output_config() {
@@ -64,75 +64,4 @@ impl AsRef<cpal::Device> for Device {
             Device::Output(device) => device,
         }
     }
-}
-
-/// Log all supported stream configs for both input and output devices.
-fn log_stream_configs(
-    // Iterator returned by [cpal::Device::supported_input_configs] or [cpal::Device::supported_output_configs].
-    configs: Result<
-        impl Iterator<Item = cpal::SupportedStreamConfigRange>,
-        cpal::SupportedStreamConfigsError,
-    >,
-    // "output" or "input"
-    cfg_type: &str,
-    // Device index in relation to the iterator returned by [cpal::Host::devices]
-    device_index: usize,
-) {
-    match configs {
-        Ok(configs) => {
-            let mut configs = configs.peekable();
-            if configs.peek().is_some() {
-                debug!("    All supported {cfg_type} stream configs:");
-                for (config_index, config) in configs.enumerate() {
-                    debug!(
-                        "      {}.{}. {:?}",
-                        device_index + 1,
-                        config_index + 1,
-                        config
-                    );
-                }
-            }
-        }
-        Err(e) => {
-            debug!("Error retrieving {cfg_type} stream configs: {:?}", e);
-        }
-    };
-}
-
-pub fn get_output_audio_devices() -> Option<Vec<Device>> {
-    let mut result = Vec::new();
-    debug!("Supported hosts:\n  {:?}", cpal::ALL_HOSTS);
-    let available_hosts = cpal::available_hosts();
-    debug!("Available hosts:\n  {:?}", available_hosts);
-
-    for host_id in available_hosts {
-        debug!("{}", host_id.name());
-        let host = cpal::host_from_id(host_id).unwrap();
-
-        let default_out = host.default_output_device().map(|e| e.name().unwrap());
-        debug!("  Default Output Device:\n    {:?}", default_out);
-
-        let default_in = host.default_input_device().and_then(|e| e.name().ok());
-        debug!("  Default Input Device:\n    {:?}", default_in);
-
-        let devices = host.devices().unwrap();
-        debug!("  Devices: ");
-        for (device_index, device) in devices.enumerate() {
-            debug!("  {}. \"{}\"", device_index + 1, device.name().unwrap());
-            // List all of the supported stream configs per device.
-            log_stream_configs(device.supported_output_configs(), "output", device_index);
-            log_stream_configs(device.supported_input_configs(), "input", device_index);
-            if let Some(device) = Device::from_device(device) {
-                result.push(device);
-            }
-        }
-    }
-
-    Some(result)
-}
-
-pub fn get_default_audio_output_device() -> Option<Device> {
-    cpal::default_host()
-        .default_output_device()
-        .map(Device::Output)
 }
